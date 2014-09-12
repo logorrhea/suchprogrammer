066b437c8a44c774ed30d7e2d1429d653058a0dd
Tyler Funk <tfunk@fastmail.fm>
2014-09-12 15:37:09 -0500

    Trying to change the timing of the websocket events.

--- public/js/controllers/SocketController.js
+++ public/js/controllers/SocketController.js
@@ -4,20 +4,21 @@
 angular.module('Gourcey').controller('SocketController', ['$scope', 'WebSocket',
     function($scope, WebSocket) {
 
-        $scope.loading = true;
+        $scope.loading = false;
         $scope.percentComplete = 0;
-        
-        WebSocket.onopen(function() {
-            WebSocket.send('message');
-        });
 
-        WebSocket.onmessage(function(event) {
-            $scope.percentComplete = event.data + "%";
-        });
-
-        WebSocket.onclose(function() {
-            $scope.loading = false;
-        });
+        $scope.openSocketConnection = function() {
+            $scope.loading = true;
+            WebSocket.onopen(function() {
+                WebSocket.send('message');
+            });
+            WebSocket.onmessage(function(event) {
+                $scope.percentComplete = event.data + "%";
+            });
+            WebSocket.onclose(function() {
+                $scope.loading = false;
+            });
+        };
 
     }
 ]);
